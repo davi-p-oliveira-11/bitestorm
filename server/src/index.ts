b5eef1652c4a3c66853694f5e2f5a82a3c5a8b06
Davi P. Oliveira <dpo.dev11@gmail.com>
2026-02-06 03:48:59 -0300

    chore(config): setup MongoDB connection and initialize server database layer

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,15 +2,20 @@ import express from "express";
 import type { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Connected to database !"));
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.get("/test", async (req: Request, res: Response) => {
-   res.json({ message: "Hello !"});   
+  res.json({ message: "Hello !" });
 });
 
 app.listen(8000, () => {
-  console.log("Server started on localhost:8000");    
+  console.log("Server started on localhost:8000");
 });
