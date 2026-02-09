0915d2ad9a2f4daaec48cec82b5a7de62d4d20f2
Davi P. Oliveira <dpo.dev11@gmail.com>
2026-02-09 20:16:01 -0300

    feat(auth): complete Auth0 authentication validation flow

--- client/src/AppRoutes.tsx
+++ client/src/AppRoutes.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/layout";
 import HomePage from "./pages/Homepage";
+import AuthCallbackPage from "./pages/AuthCallBackPage";
 
 const AppRoutes = () => {
   return (
@@ -13,6 +14,7 @@ const AppRoutes = () => {
           </Layout>
         }
       />
+      <Route path="/auth-callback" element={<AuthCallbackPage />} />
       <Route path="/user-profile" element={<span>User profile page</span>} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
--- client/src/auth/Auth0ProviderWithNavigate.tsx
+++ client/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,4 +1,3 @@
-import { useCreateMyUser } from "@/api/MyUserApi";
 import { type AppState, Auth0Provider, User } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 
@@ -7,26 +6,21 @@ type Props = {
 };
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
-  const { createUser } = useCreateMyUser();
+ 
   const navigate = useNavigate();
 
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
-{/* const audience = import.meta.env.VITE_AUTH0_AUDIENCE; */}
+  const audience = import.meta.env.VITE_AUTH0_AUDIENCE; 
 
-  if (!domain || !clientId || !redirectUri ) {
+  if (!domain || !clientId || !redirectUri || !audience) {
     throw new Error("unable to initialize auth");
   }
 
   const onRedirectCallback = (appState?: AppState, user?: User) => {
-    if(user?.sub && user?.email) {
-      createUser({ auth0Id: user.sub, email: user.email });
-    }
-    {/*
     navigate(appState?.returnTo || "/auth-callback");
     console.log("USER", user)
-    */}
   };
 
   return (
@@ -35,6 +29,7 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
       clientId={clientId}
       authorizationParams={{
         redirect_uri: redirectUri,
+        audience
       }}
       onRedirectCallback={onRedirectCallback}
     >
