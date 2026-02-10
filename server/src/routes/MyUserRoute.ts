fc22242bd1ab77dd903463481104199c20de238f
Davi P. Oliveira <dpo.dev11@gmail.com>
2026-02-09 23:27:54 -0300

    feat(user): implement user update API with validation and auth middleware

--- server/src/routes/MyUserRoute.ts
+++ server/src/routes/MyUserRoute.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import MyUserController from "../controllers/MyUserController.js";
-import { jwtCheck } from "../middleware/auth.js";
+import { jwtCheck, jwtParse } from "../middleware/auth.js";
+import { validateMyUserRequest } from "../middleware/validation.js";
 
 const router = express.Router();
 
 router.post("/", jwtCheck, MyUserController.createCurrentUser);
+router.put("/", jwtCheck, jwtParse, validateMyUserRequest, MyUserController.updateCurrentUser)
 
 export default router;
