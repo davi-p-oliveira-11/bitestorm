cbe1e484233e96b31d7abaac7daa9ef775b4e16c
Davi P. Oliveira <dpo.dev11@gmail.com>
2026-02-11 17:32:00 -0300

    fix(server): handle possible null _id in auth middleware

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -7,15 +7,15 @@ declare global {
    namespace Express {
       interface Request {
          userId: string;
-         auth0Id: string;   
+         auth0Id: string;
       }
-   }  
+   }
 }
 
 export const jwtCheck = auth({
-  audience: process.env.AUTH0_AUDIENCE,
-  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
-  tokenSigningAlg: 'RS256',
+   audience: process.env.AUTH0_AUDIENCE,
+   issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
+   tokenSigningAlg: 'RS256',
 });
 
 export const jwtParse = async (req: Request, res: Response, next: NextFunction) => {
@@ -28,19 +28,26 @@ export const jwtParse = async (req: Request, res: Response, next: NextFunction) 
    const token = authorization.split(" ")[1];
 
    try {
-     const decoded = jwt.decode(token) as jwt.JwtPayload;
-     const auth0Id = decoded.sub;
+      const decoded = jwt.decode(token) as jwt.JwtPayload | null;
 
-     const user = await User.findOne({ auth0Id })
+      if (!decoded || !decoded.sub) {
+         return res.sendStatus(401);
+      }
 
-     if (!user) {
-       return res.sendStatus(401);
-     }
+      const auth0Id = decoded.sub;
 
-     req.auth0Id = auth0Id as string;
-     req.userId = user._id.toString();
-     next();
+      const user = await User.findOne({ auth0Id });
+
+      if (!user || !user._id) {
+         return res.sendStatus(401);
+      }
+
+      req.auth0Id = auth0Id as string;
+      req.userId = user._id.toString();
+
+      next();
+
    } catch (error) {
-     return res.sendStatus(401); 
+      return res.sendStatus(401);
    }
 }
