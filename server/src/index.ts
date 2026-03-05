981ceb66869fe57d534b1af05b662b7f250318d8
Davi P. Oliveira <dpo.dev11@gmail.com>
2026-03-04 23:02:49 -0300

    feat(server): implement restaurant creation API with validation and routing

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,7 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute.js"
 import { v2 as cloudinary} from "cloudinary"
+import myRestaurantRoute from "./routes/MyRestaurantRoute.js"
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -25,6 +26,7 @@ app.get("/health", async (req: Request, res: Response) => {
 });
 
 app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restaurant", myRestaurantRoute)
 
 app.listen(8000, () => {
   console.log("Server started on localhost:8000");
---
+++ server/src/routes/MyRestaurantRoute.ts
@@ -0,0 +1,28 @@
+import express from "express"
+import multer from "multer";
+import MyRestaurantController from "../controllers/MyRestaurantController.js";
+import { jwtCheck, jwtParse } from "../middleware/auth.js";
+import { validateMyRestaurantRequest } from "../middleware/validation.js";
+
+const router = express.Router();
+
+const storage = multer.memoryStorage();
+const upload = multer({
+   storage: storage,
+   limits: {
+     fileSize: 5 * 1024 * 1024,
+   },   
+})
+
+// /api/my/restaurant
+
+router.post(
+      "/", 
+      upload.single("imageFIle"),
+      validateMyRestaurantRequest,
+      jwtCheck,
+      jwtParse,
+      MyRestaurantController.createMyRestaurant
+);
+
+export default router;
